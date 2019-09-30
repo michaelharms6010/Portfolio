4ca3d59e3073f6fceb2aecd0500fade911de430e
admin <admin@admins-MacBook-Pro.local>
2019-09-29 19:28:53 -0700

    fixed about section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,14 +9,32 @@ function AboutMe() {
           <div id="AboutMe" className="about-container">
 
                <div className="about-content">
-                    <h2 className="about-title">README.md</h2>
-                    <p>I currently reside in Santa Barbara, California, and frequent between here and the Bay Area. After graduating from University of California, Santa Barbara with a degree in Biology, I threw in my scrubs to study web development at Lambda School.</p>
-                    <p>I hope to use the experience I’ve gained from working with people in medical and professional settings (and of course my new CS skills) to join a startup centered around biomedical or lifestyle products.</p>
-                    <p>I also dabble in hot yoga</p>
-               </div>
-               <div className="about-picture">
                     <img src={CroppedProfile} />
+                    {/* <h2>README.md</h2> */}
+                    
                </div>
+               
+               <div className="about-container-content">
+                    <div className="about-content-container">
+                         <h5 className="about-subtitle">Where I've been</h5>
+                         <p>Growing up, I spent <span className="about-accents">6 years studying Buddhism</span> with monks and was class president in high school. In college, I became a brother of Sigma Chi, the recruitment chairman of Mu Delta, and shadowed the leading pioneer of <span className="about-accents">ketamine psychotherapy.</span> </p>
+                         <p>I'm proud to say these experiences have given me a unique skillset and perspective on life. It’s really <span className="about-italics">shaped who I am today</span>.</p>
+                    </div>
+
+                    <div className="about-content-container">
+                         <h5 className="about-subtitle">What I'm doing</h5>
+                         <p>After graduating from University of California, Santa Barbara with a <span className="about-accents">degree in Biology</span>, I threw in my scrubs to study<span className="about-accents"> full stack web development</span> at Lambda School. If I’ve learned anything from coding thus far, it’s that <span className="about-italics">languages are always changing and evolving - and I plan on doing the same.</span></p>
+                         {/* <p>And I'd like to say surfing is my hobby, but to be honest with you after nearly drowning twice - once during a microburst in the middle of the ocean and again while looking for bioluminescent algae - I've decided to give it a little break. </p> */}
+                    </div>
+
+                    <div className="about-content-container">
+                         <h5 className="about-subtitle">What I hope to do</h5>
+                         <p>To keep it simple, I want to keep finding <span className="about-accents">opportunities to grow</span>, both personally and professionally. I hope to do meaningful work for a company that provides a meaningful product. If we’re getting to specifics, a smaller to medium-sized company that’s fast paced and allows room for growth would be a dream. </p>
+                         <p>And <span className="about-italics">maybe</span> I’ll get back into rowing. </p>
+                    </div>
+               </div>
+
+           
           </div>
      )
 }
