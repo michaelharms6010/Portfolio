7eacc0434440bac4ce16d56861fa334c7b73cd07
Michael Harms <michael.harms6010@gmail.com>
2020-02-12 21:56:41 -0600

    removes routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import './App.css';
 import AboutMe from './components/AboutMe'
@@ -10,10 +9,10 @@ import Contact from './components/Contact'
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={LandingPage} />
-      <Route path="/" component={AboutMe} />
-      <Route path="/" component={Projects} />
-      <Route path="/" component={Contact} />
+      <LandingPage />
+      <AboutMe />
+      <Projects />
+      <Contact />
     </div>
   );
 }
